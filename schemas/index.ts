23e8625ede8bfbb06a3b6c8ebabbc3b225d36d79
younggsad <vel007evgeniy@gmail.com>
2025-12-07 16:29:53 +0300

    Разработка 3

--- schemas/index.ts
+++ schemas/index.ts
@@ -13,3 +13,20 @@ export const LoginSchema = z.object({
             message: "Password cannot be empty"
         }),
 });
+
+export const RegisterSchema = z.object({
+    email: z.string()
+        .min(1, {
+            message: "Email cannot be empty"
+        })
+        .email({
+            message: "Please enter a valid email address"
+        }),
+    password: z.string()
+        .min(6, {
+            message: "Password shold contain minimum 6 elements"
+        }),
+    name: z.string().min(1, {
+        message: "Name is required",
+    }),
+});

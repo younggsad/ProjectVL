ac4fb773dfdcee31d73036d6a655b3fb38b8f88c
younggsad <vel007evgeniy@gmail.com>
2025-12-08 00:57:49 +0300

    Разработка 4

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,14 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 declare global {
     var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
+const adapter = new PrismaPg(pool);
+
+export const db = globalThis.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
